b6af126fbda4e77917679bfb11649d394ee788d5
Kirk <kirk@broadhur.st>
2017-04-03 12:17:02 -0500

    some matrix / diagonal work

--- reverse_diagonals.py
+++ reverse_diagonals.py
@@ -12,3 +12,28 @@ def reverseOnDiagonals(matrix):
     return matrix
 
 
+def swapDiagonals(matrix):
+    l = len(matrix)
+    z = (l if l % 2 == 0 else l + 1) / 2
+    for i in range(0, l):
+        t = matrix[i][i]
+        matrix[i][i] = matrix[i][l - 1 - i]
+        matrix[i][l - 1 - i] = t
+    return matrix
+
+
+def drawRectangle(canvas, rectangle):
+    w = rectangle
+    p = [(w[1], w[0]), (w[1], w[2]), (w[3], w[0]), (w[3], w[2])]
+
+    for r in range(len(canvas)):
+        for c in range(len(canvas[r])):
+            if (r, c) in p:
+                canvas[r][c] = '*'
+            elif r in [i[0] for i in p] and w[0] < c < w[2]:
+                canvas[r][c] = '-'
+            elif c in [i[1] for i in p] and w[1] < r < w[3]:
+                canvas[r][c] = '|'
+
+    return canvas
+
